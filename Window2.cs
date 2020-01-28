25f92990d85a73adf002a7835beda07250a2fa10
Alexeika <43091324+Alexeika@users.noreply.github.com>
2020-01-28 16:41:37 +0300

    Add files via upload

--- Window2.cs
+++ Window2.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp4
 {
-    public partial class Form1 : Form
+    public partial class Window2 : Form
     {
-        public Form1()
+        public Window2()
         {
             InitializeComponent();
         }
