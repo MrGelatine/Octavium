8186021069400e9ffbd6f15e87db1e86d4fa2a56
Amalgam <dan_28_00@mail.ru>
2020-02-09 12:32:20 +0300

    add file processing func

--- InterfaceFuncs.cs
+++ InterfaceFuncs.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp3
 {
     class InterfaceFuncs
     {
-        public static void CreateSheet(string engien_path,string data_path, string gallery_path)
+        public static void GetAndAddData(string engien_path = @"C:\Users\Денис\Desktop\Rep\Octavium\sheet.exe", string sheet_path = @"C:\Users\Денис\Desktop\Rep\Octavium\Gallery\Sheets", string data_storage_path = @"C:\Users\Денис\Desktop\Rep\Octavium\Gallery\Piano data")
+        {
+            string midi_data_path = null;
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    midi_data_path = dialog.SelectedPath;
+                }
+            }
+            CreateSheet(engien_path, midi_data_path, sheet_path);
+            MIDIFuncs.SaveToData(new MIDINotesData(midi_data_path), data_storage_path);
+        }
+        public static void CreateSheet(string engien_path,string data_path, string sheet_path)
         {
             var name = Regex.Match(data_path, @"[\\]+[^\\]+.mid").Value.Remove(0, 1);
             var sheetMaker = new ProcessStartInfo(engien_path, $"{data_path} {name}");
@@ -27,7 +41,7 @@ namespace WindowsFormsApp3
             {
                 if (Regex.IsMatch(line, ".png"))
                 {
-                    File.Move(line, $@"{gallery_path}\{Regex.Match(line, @"[\\]+[^\\]+.png").Value.Remove(0, 1)}");
+                    File.Move(line, $@"{sheet_path}\{Regex.Match(line, @"[\\]+[^\\]+.png").Value.Remove(0, 1)}");
                 }
             }
 
