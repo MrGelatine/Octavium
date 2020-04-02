b5085123e6cd947f5873b467a475fa36174ff64c
Melano2011 <ahmedmelano2011@gmail.com>
2020-04-02 20:10:23 +0300

    Playing Mid File

---
+++ Play.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Play
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(103, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(197, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Play
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(300, 150);
+            this.MinimumSize = new System.Drawing.Size(300, 150);
+            this.Name = "Play";
+            this.Text = "Play";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Play.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanford.Multimedia.Midi;
+using Sanford.Multimedia.Midi.UI;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Play : Form
+    {
+        int time = 0;
+        MIDINotesData My;
+        bool pathcheck = false;
+        private OutputDevice outDevice;
+        private int outDeviceID = 0;
+        private OutputDeviceDialog outDialog = new OutputDeviceDialog();
+        public Play(String Path = "")
+        {
+            if (Path != "")
+            {
+                My = new MIDINotesData(MIDIFuncs.UnpackDataToNote(Path));
+                pathcheck = true;
+                if ((int)My.flowkeys[0].time > 1000)
+                    time = (int)My.flowkeys[0].time - 1000;
+            }
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (OutputDevice.DeviceCount == 0)
+            {
+                MessageBox.Show("No MIDI output devices available.", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Close();
+            }
+            else
+            {
+                try
+                {
+                    outDevice = new OutputDevice(outDeviceID);
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                    Close();
+                }
+            }
+
+            base.OnLoad(e);
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            timer1.Stop();
+            time = 0;
+            if (outDevice != null)
+            {
+                outDevice.Dispose();
+            }
+
+            outDialog.Dispose();
+
+            base.OnClosed(e);
+        }
+        private void PlayingMid() {
+            foreach (var x in My.flowkeys) {
+                if ((x.time / (timer1.Interval) == (time / timer1.Interval))) {
+                    outDevice.Send(new ChannelMessage(ChannelCommand.NoteOn, 0, x.pos+20, 127));
+                }
+                if ((x.time + x.length) / timer1.Interval == (time / timer1.Interval)) {
+                    outDevice.Send(new ChannelMessage(ChannelCommand.NoteOff, 0, x.pos + 20, 0));
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          if(pathcheck)
+              PlayingMid();
+            time += timer1.Interval; //increase the time
+            Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
